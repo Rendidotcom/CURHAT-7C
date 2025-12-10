9a6adfcc5a33c38522b25151bfacf65655cd80a4
Rendidotcom <holis112@gmail.com>
2025-12-11 05:53:36 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// =======================
+// CONFIG API BACKEND
+// =======================
+export const API_URL =
+  "https://script.google.com/macros/s/AKfycbznTP1rdkrz-5P2nc9eY6C0Zbn7VdRn9D5TT2cKFXQciEyLecq3x30DO5h6A9hhx_T4tA/exec";
