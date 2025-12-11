5cf44b3bf112948ed721de13af7ba8e6e89a1697
Rendidotcom <holis112@gmail.com>
2025-12-11 22:07:39 +0700

    tes curhat 35

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // config.js — FINAL CURHAT 7C
-window.API_URL = "https://script.google.com/macros/s/AKfycbwT8cCL8pwW2ABRneD7QZS8AepPpcKqXx3uzLjqPCfNxLnn58Q_lf_HK8AaWNZGSwnz/exec";
+window.API_URL = "https://script.google.com/macros/s/AKfycbyg5ZYnxhjRyXZJ4qukPwK7fuW7L4pWkGjA6Gag8VVMjWyi_gh1WySdrIkhmbvigGA1Og/exec";
