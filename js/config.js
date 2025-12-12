fa48d6fbdac932ab585cbcd3b891d3c6f84f226c
Rendidotcom <holis112@gmail.com>
2025-12-13 03:43:01 +0700

    tes buka

--- js/config.js
+++ js/config.js
@@ -1,3 +1,3 @@
 // config.js — FINAL VALID PATH
 // API endpoint = Vercel serverless function
-const API_URL = "https://script.google.com/macros/s/AKfycbxPIx7gC6hclc-JEBcMqIooAcXNSC2JbZkSRs3CYmZPbgCTKphMquVBNCSPpxVy-dwkgw/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzoybyvDG8jYG1Tff0XOYwt-gLbFMmx-S4oiqhWTW8O50lak5BcmxKr1SzGSRlVggEJmQ/exec";
