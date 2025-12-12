37ad1caaea39cb514c7077fdd2d4e807700a5f32
Rendidotcom <holis112@gmail.com>
2025-12-13 04:28:58 +0700

    tes curhat 52

--- js/config.js
+++ js/config.js
@@ -1,3 +1,4 @@
 // config.js — FINAL VALID PATH
 // API endpoint = Vercel serverless function
-const API_URL = "https://script.google.com/macros/s/AKfycbzoybyvDG8jYG1Tff0XOYwt-gLbFMmx-S4oiqhWTW8O50lak5BcmxKr1SzGSRlVggEJmQ/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbzoybyvDG8jYG1Tff0XOYwt-gLbFMmx-S4oiqhWTW8O50lak5BcmxKr1SzGSRlVggEJmQ/exec
+";
