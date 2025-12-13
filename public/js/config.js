94e73de5b418dc6f4b66f5500fdf8cb16199b0f1
Rendidotcom <holis112@gmail.com>
2025-12-14 05:27:49 +0700

    tes curhat

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 // config.js — FINAL
 (function () {
   const API_URL =
-    "https://script.google.com/macros/s/AKfycbxzwQVLPqhIsnUXVltEVqYb6cvxJT3GCA7HUvrCqFhlyXcUHCs2Ndu4qPy2JHQPGt1cvA/exec";
+    "https://script.google.com/macros/s/AKfycbx8bqoirLDJNBsYV5wmT_RwEarvGXhJn65N3zD7PdQZNjqv4-3-qbxxvYxWO7_eZv0cqg/exec";
 
   window.API_URL = API_URL;
 })();
