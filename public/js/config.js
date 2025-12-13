3c3ca364c7c8dff15cc4cab9cde81afffc77a379
Rendidotcom <holis112@gmail.com>
2025-12-14 05:13:57 +0700

    tes curhat 63

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 // config.js — FINAL
 (function () {
   const API_URL =
-    "https://script.google.com/macros/s/AKfycbznxY3lMt6yVJFz_o5pvOPTzfuQdKLDLrXah2k_hTeQIzH0H3YZltuj_LqLsFVtJks8Ig/exec";
+    "https://script.google.com/macros/s/AKfycbxzwQVLPqhIsnUXVltEVqYb6cvxJT3GCA7HUvrCqFhlyXcUHCs2Ndu4qPy2JHQPGt1cvA/exec";
 
   window.API_URL = API_URL;
 })();
