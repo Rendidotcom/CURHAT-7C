d20c43f87e2d5e7bb00324ccaa426f6f51e0ca22
Rendidotcom <holis112@gmail.com>
2026-01-19 12:02:13 +0700

    v13

--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,7 @@
 // config.js — FINAL
 (function () {
   const API_URL =
-    "https://script.google.com/macros/s/AKfycbxX-yrW_YP0CWEZzKoe-ZLmTEYYAVwnS6pZYAyicMQh2suIC7RD_Y91EbIvEJKKEe9k5g/exec";
+    "https://script.google.com/macros/s/AKfycbzRiBYuKfPG0igR_vTndmUNqY3td8fH0edsFA7d-V2bDhUAeDqgoicNjgud2sQ4pT-tsA/exec";
 
   window.API_URL = API_URL;
 })();
